0b98c1509e200f5958f91729968909761af8ed5e
Reshma Konjari <reshmakonjari112@gmail.com>
2018-08-07 18:39:22 +0530

    Feat: add fetaure column

--- examples/test_tensor.py
+++ examples/test_tensor.py
@@ -12,4 +12,17 @@ from IPython import display
 california_housing_data = pd.read_csv("https://dl.google.com/mlcc/mledu-datasets/california_housing_train.csv", sep=",")
 print california_housing_data
 
-california_housing_data = california_housing_data.reindex(np.random.permutation(california_housing_data))
+california_housing_data = california_housing_data.reindex(np.random.permutation(california_housing_data.index))
+# print california_housing_data
+
+california_housing_data['median_house_value']/=1000.0
+# print california_housing_data
+
+print california_housing_data.describe()
+
+#---------- model-------------
+input_feature = california_housing_data[['total_rooms']]
+print input_feature
+
+feature_column = [tf.feature_cloumn.numeric_column('total_rooms')]
+print feature_column
